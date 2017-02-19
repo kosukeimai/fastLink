a208c645d7e88705851ed41adede712e528c5da7
Ben Fifield <benfifield@gmail.com>
2017-02-19 13:30:54 -0500

    adding ncores argument to fastLink()

--- fastLink/R/fastLink.R
+++ fastLink/R/fastLink.R
@@ -10,11 +10,20 @@
 #' @param partial_match A vector of booleans, indicating whether to use
 #' partial matching or exact matching when determining matching patterns on
 #' each variable.
+#' @param n.cores Number of cores to parallelize over. Default is NULL.
 #' @param tol.em Convergence tolerance for the EM Algorithm. Default is 1e-04.
 #' @param tol.match Convergence tolerance for determining matches. Default is 1e-07.
+#' @param verbose Whether to print elapsed time for each step. Default is FALSE.
+#'
+#' @author Ted Enamorado <ted.enamorado@gmail.com>, Ben Fifield <benfifield@gmail.com>, and Kosuke Imai
 #'
 #' @export
-fastLink <- function(df_a, df_b, varnames, partial_match, tol.em = 1e-04, tol.match = 1e-07){
+fastLink <- function(df_a, df_b, varnames, partial_match, n.cores = NULL, tol.em = 1e-04, tol.match = 1e-07, verbose = FALSE){
+
+    cat("\n")
+    cat(c(paste(rep("=", 20), sep = "", collapse = ""), "\n"))
+    cat("fastLink(): Fast Probabilistic Record Linkage\n")
+    cat(c(paste(rep("=", 20), sep = "", collapse = ""), "\n\n"))
 
     if(length(varnames) != length(partial_match)){
         stop("There must be one entry in partial_match for each entry in varnames.")
@@ -22,13 +31,18 @@ fastLink <- function(df_a, df_b, varnames, partial_match, tol.em = 1e-04, tol.ma
 
     ## Create gammas
     cat("Calculating matches for each variable.\n")
+    start <- Sys.time()
     gammalist <- vector(mode = "list", length = length(varnames))
     for(i in 1:length(gammalist)){
         if(partial_match[i]){
-            gammalist[[i]] <- gammaCKpar(df_a[,varnames[i]], df_b[,varnames[i]])
+            gammalist[[i]] <- gammaCKpar(df_a[,varnames[i]], df_b[,varnames[i]], n.cores = n.cores)
         }else{
-            gammalist[[i]] <- gammaKpar(df_a[,varnames[i]], df_b[,varnames[i]])
+            gammalist[[i]] <- gammaKpar(df_a[,varnames[i]], df_b[,varnames[i]], n.cores = n.cores)
         }
+    }
+    end <- Sys.time()
+    if(verbose){
+        cat("Calculating matches for each variable took ", difftime(end, start, units = "mins"), " minutes.\n\n")
     }
 
     ## Get row numbers
@@ -37,11 +51,21 @@ fastLink <- function(df_a, df_b, varnames, partial_match, tol.em = 1e-04, tol.ma
 
     ## Get counts for zeta parameters
     cat("Getting counts for zeta parameters.\n")
-    counts <- tableCounts(gammalist, nr1 = nr_a, nr2 = nr_b)
+    start <- Sys.time()
+    counts <- tableCounts(gammalist, nr1 = nr_a, nr2 = nr_b, n.cores = n.cores)
+    end <- Sys.time()
+    if(verbose){
+        cat("Getting counts for zeta parameters took ", difftime(end, start, units = "mins"), " minutes.\n\n")
+    }
 
     ## Run EM algorithm
     cat("Running the EM algorithm.\n")
+    start <- Sys.time()
     resultsEM <- emlinkMAR(patterns = counts, tol = tol.em)
+    end <- Sys.time()
+    if(verbose){
+        cat("Running the EM algorithm took ", difftime(end, start, units = "secs"), " seconds.\n\n")
+    }
 
     ## Get output
     EM <- data.frame(cbind(resultsEM$patterns.w))
@@ -52,8 +76,14 @@ fastLink <- function(df_a, df_b, varnames, partial_match, tol.em = 1e-04, tol.ma
 
     ## Get matches
     cat("Getting the indices of estimated matches.\n")
+    start <- Sys.time()
     matches <- matchesLink(gammalist, nr1 = nr_a, nr2 = nr_b,
-                           em = resultsEM, cut = match.ut)
+                           em = resultsEM, cut = match.ut,
+                           n.cores = n.cores)
+    end <- Sys.time()
+    if(verbose){
+        cat("Getting the indices of estimated matches took ", difftime(end, start, units = "mins"), " minutes.\n\n")
+    }
     colnames(matches) <- c("inds_a", "inds_b")
 
     return(matches)
