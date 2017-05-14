2a1ed785d7c8d38394ddbcb0f3fea724470823bf
Ben Fifield <benfifield@gmail.com>
2017-05-13 20:16:35 -0400

    up dedupe

--- R/dedupeMatches.R
+++ R/dedupeMatches.R
@@ -1,11 +1,10 @@
 #' dedupeMatches
 #'
-#' Dedupe matched dataframes using the linear sum assignment problem
-#' methodology.
+#' Dedupe matched dataframes.
 #'
 #' @usage dedupeMatches(matchesA, matchesB, EM,
 #' matchesLink, varnames, stringdist.match, partial.match,
-#' cut.a = 0.92, cut.p = 0.88)
+#' linprog, cut.a = 0.92, cut.p = 0.88)
 #' @param matchesA A dataframe of the matched observations in
 #' dataset A, with all variables used to inform the match.
 #' @param matchesB A dataframe of the matched observations in
@@ -20,6 +19,8 @@
 #' @param partial.match A vector of booleans, indicating whether to include
 #' a partial matching category for the string distances. Must be same length
 #' as varnames. Default is FALSE for all variables.
+#' @param linprog Whether to implement Winkler's linear programming solution to the deduplication
+#' problem. Default is false.
 #' @param cut.a Lower bound for full string-distance match, ranging between 0 and 1. Default is 0.92
 #' @param cut.p Lower bound for partial string-distance match, ranging between 0 and 1. Default is 0.88
 #'
@@ -35,6 +36,7 @@
 #' @importFrom stringdist stringdist
 dedupeMatches <- function(matchesA, matchesB, EM, matchesLink,
                           varnames, stringdist.match, partial.match,
+                          linprog = FALSE,
                           cut.a = 0.92, cut.p = 0.88){
 
     ## --------------
@@ -105,63 +107,125 @@ dedupeMatches <- function(matchesA, matchesB, EM, matchesLink,
     matchesB$idA <- matchesA$idA
     matchesA$idB <- matchesB$idB
 
-    ## Find duplicates
-    dupA <- duplicated(matchesA$idA)
-    dupB <- duplicated(matchesA$idA, fromLast = T)
-    matchesA$dupA <- ifelse(dupA == 1 | dupB == 1, 1, 0)
+    if(!linprog){
 
-    dupA <- duplicated(matchesB$idB)
-    dupB <- duplicated(matchesB$idB, fromLast = T)
-    matchesA$dupB <- ifelse(dupA == 1 | dupB == 1, 1, 0)
+        ## Step 1: Find max zeta for each observation in dataset A:
+        ## Merge in maximum zeta for each observation in dataset A
+        temp <- as.matrix(tapply(matchesA$zeta.j, matchesA$idA, max, na.rm = T))			
+        temp <- data.frame(cbind(as.numeric(rownames(temp)), as.numeric(temp)))
+        names(temp) <- c("idA", "zeta.max")
+        matchesA <- merge(matchesA, temp, by = "idA")
 
-    ## Split into dupes, not dups
-    dups <- subset(matchesA, dupA == 1 | dupB == 1)
-    nodups <- subset(matchesA, dupA == 0 & dupB == 0)
+        ## Calculate difference
+        matchesA <- matchesA[order(matchesA$roworder), ]			
+        matchesB <- matchesB[order(matchesB$roworder), ]			
+        matchesA$rm <- abs(matchesA$zeta.j - matchesA$zeta.max)
+        rm <- which(matchesA$rm == 0)
 
-    dups$idA.t <- as.numeric(as.factor(dups$idA))
-    dups$idB.t <- as.numeric(as.factor(dups$idB))
+        ## Subset down to max zetas
+        matchesA <- matchesA[rm, ]
+        matchesB <- matchesB[rm, ]
 
-    nr <- max(dups$idA.t)
-    nc <- max(dups$idB.t)
-    dim <- max(nr, nc)
+        ## Step 2: Find max zeta for each observation in dataset B, if in first subset:
+        ## Merge in maximum zeta for each observation in dataset B
+        temp <- as.matrix(tapply(matchesB$zeta.j, matchesB$idB, max, na.rm = T))			
+        temp <- data.frame(cbind(as.numeric(rownames(temp)), as.numeric(temp)))
+        names(temp) <- c("idB", "zeta.max")
+        matchesB <- merge(matchesB, temp, by = "idB")
 
-    ## Create adjacency matrix to id duplicates
-    mat.adj <- sparseMatrix(i = dups$idA.t, j = dups$idB.t, x = dups$zeta.j,
-                            dims = c(dim, dim))
-    mat.adj <- as.matrix(mat.adj)
+        ## Calculate difference
+        matchesA <- matchesA[order(matchesA$roworder), ]			
+        matchesB <- matchesB[order(matchesB$roworder), ]			
+        matchesB$rm <- abs(matchesB$zeta.j - matchesB$zeta.max)
+        rm <- which(matchesB$rm == 0)
 
-    ## Solve linear sum assignment problem
-    T1 <- suppressWarnings(assignment(-mat.adj))
-    temp.0 <- cbind(1:dim, T1$perm)
-    n1 <- which(rowSums(mat.adj) == 0)
-    n2 <- which(colSums(mat.adj) == 0)
+        ## Subset down to max zetas
+        matchesA <- matchesA[rm, ]
+        matchesB <- matchesB[rm, ]
 
-    if(length(n1) > 0) {
-        temp.0 <- temp.0[-n1, ]
+        ## Step 3: Break remaining ties
+        ## Find remaining duplicates in A
+        d1 <- duplicated(matchesA$idA)
+        d2 <- duplicated(matchesA$idA, fromLast = T)
+        matchesA$dA <- ifelse((d1 + d2) > 0, 1, 0)
+        matchesB$dA <- ifelse((d1 + d2) > 0, 1, 0)
+
+        ## Draw uniform to break tie, and merge in 
+        matchesA$uni <- runif(nrow(matchesA))			
+        temp <- as.matrix(tapply(matchesA$uni, matchesA$idA, max, na.rm = T))			
+        temp <- data.frame(cbind(as.numeric(rownames(temp)), as.numeric(temp)))
+        names(temp) <- c("idA", "uni.max")
+        matchesA <- merge(matchesA, temp, by = "idA")
+        matchesA <- matchesA[order(matchesA$roworder), ]			
+        matchesB <- matchesB[order(matchesB$roworder), ]			
+        matchesA$rm <- abs(matchesA$uni - matchesA$uni.max)
+        rm <- which(matchesA$rm == 0)
+
+        ## Subset down to broken tie
+        matchesA <- matchesA[rm, ]
+        matchesB <- matchesB[rm, ]
+        
+    }else{
+
+        ## Find duplicates
+        dupA <- duplicated(matchesA$idA)
+        dupB <- duplicated(matchesA$idA, fromLast = T)
+        matchesA$dupA <- ifelse(dupA == 1 | dupB == 1, 1, 0)
+
+        dupA <- duplicated(matchesB$idB)
+        dupB <- duplicated(matchesB$idB, fromLast = T)
+        matchesA$dupB <- ifelse(dupA == 1 | dupB == 1, 1, 0)
+
+        ## Split into dupes, not dups
+        dups <- subset(matchesA, dupA == 1 | dupB == 1)
+        nodups <- subset(matchesA, dupA == 0 & dupB == 0)
+
+        dups$idA.t <- as.numeric(as.factor(dups$idA))
+        dups$idB.t <- as.numeric(as.factor(dups$idB))
+
+        nr <- max(dups$idA.t)
+        nc <- max(dups$idB.t)
+        dim <- max(nr, nc)
+
+        ## Create adjacency matrix to id duplicates
+        mat.adj <- sparseMatrix(i = dups$idA.t, j = dups$idB.t, x = dups$zeta.j,
+                                dims = c(dim, dim))
+        mat.adj <- as.matrix(mat.adj)
+
+        ## Solve linear sum assignment problem
+        T1 <- suppressWarnings(assignment(-mat.adj))
+        temp.0 <- cbind(1:dim, T1$perm)
+        n1 <- which(rowSums(mat.adj) == 0)
+        n2 <- which(colSums(mat.adj) == 0)
+
+        if(length(n1) > 0) {
+            temp.0 <- temp.0[-n1, ]
+        }
+
+        if(length(n2) > 0) {
+            temp.0 <- temp.0[, -n2]
+        }
+
+        temp.0 <- data.frame(temp.0)
+        names(temp.0) <- c("idA.t", "idB.t")
+
+        ## Merge in dedupe information
+        dedup <- merge(temp.0, dups, by = c("idA.t", "idB.t"))
+        dedup$idA.t <- dedup$idB.t <- NULL
+
+        ## Combine dupes, dedupes
+        matchesA <- rbind(dedup, nodups)
+        matchesA$dupA <- matchesA$dupB <- NULL	
+        listA <- paste(matchesA$idA, matchesA$idB, sep = "-")
+        listB <- paste(matchesB$idA, matchesB$idB, sep = "-")
+        keep <- which(listB %in% listA)
+        matchesB <- matchesB[keep, ]
+
+        ## Subset down and order
+        matchesA <- matchesA[order(matchesA$idA, matchesA$idB), ]
+        matchesB <- matchesB[order(matchesB$idA, matchesB$idB), ]
+
     }
-
-    if(length(n2) > 0) {
-        temp.0 <- temp.0[, -n2]
-    }
-
-    temp.0 <- data.frame(temp.0)
-    names(temp.0) <- c("idA.t", "idB.t")
-
-    ## Merge in dedupe information
-    dedup <- merge(temp.0, dups, by = c("idA.t", "idB.t"))
-    dedup$idA.t <- dedup$idB.t <- NULL
-
-    ## Combine dupes, dedupes
-    matchesA <- rbind(dedup, nodups)
-    matchesA$dupA <- matchesA$dupB <- NULL	
-    listA <- paste(matchesA$idA, matchesA$idB, sep = "-")
-    listB <- paste(matchesB$idA, matchesB$idB, sep = "-")
-    keep <- which(listB %in% listA)
-    matchesB <- matchesB[keep, ]
-
-    ## Subset down and order
-    matchesA <- matchesA[order(matchesA$idA, matchesA$idB), ]
-    matchesB <- matchesB[order(matchesB$idA, matchesB$idB), ]
 
     ## -----------------
     ## Correct EM object
@@ -198,6 +262,4 @@ dedupeMatches <- function(matchesA, matchesB, EM, matchesLink,
     return(out)
 
 }
-
-
 
