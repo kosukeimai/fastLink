e3e3bb955fd6b1fb211770b6ab1314a1727bddc5
Ben Fifield <benfifield@gmail.com>
2017-08-03 17:55:44 -0400

    integrating cleanAddressUSPS into preprocText

--- R/preprocText.R
+++ R/preprocText.R
@@ -3,7 +3,7 @@
 #' Preprocess text data such as names and addresses.
 #'
 #' @usage preprocText(text, convert_text, tolower, soundex,
-#' convert_text_to)
+#' usps_address, convert_text_to)
 #' @param text A vector of text data to convert.
 #' @param convert_text Whether to convert text to the desired encoding, where
 #' the encoding is specified in the 'convert_text_to' argument. Default is
@@ -11,6 +11,8 @@
 #' @param tolower Whether to normalize the text to be all lowercase. Default is
 #' TRUE.
 #' @param soundex Whether to convert the field to the Census's soundex encoding.
+#' Default is FALSE.
+#' @param usps_address Whether to use USPS address standardization rules to clean address fields.
 #' Default is FALSE.
 #' @param convert_text_to Which encoding to use when converting text. Default is 'Latin-ASCII'.
 #' Full list of encodings in the \code{stri_trans_list()} function in the \code{stringi} package.
@@ -22,7 +24,7 @@
 #' @importFrom stringi stri_trans_list stri_trans_general
 #' @importFrom stringdist phonetic
 preprocText <- function(text, convert_text = TRUE, tolower = TRUE,
-                        soundex = FALSE, convert_text_to = "Latin-ASCII"){
+                        soundex = FALSE, usps_address = FALSE, convert_text_to = "Latin-ASCII"){
 
     if(!(convert_text_to %in% stri_trans_list())){
         stop("Sorry, that encoding is not included in the set of valid encodings. Please check 'stri_trans_list()' in the 'stringi' package for the full set of valid encodings.")
@@ -36,6 +38,21 @@ preprocText <- function(text, convert_text = TRUE, tolower = TRUE,
     }
     if(soundex){
         text <- phonetic(text)
+    }
+    if(usps_address){
+        text <- gsub(" avenue", " ave", text)
+        text <- gsub(" boulevard", " blvd", text)
+        text <- gsub(" circle", " cir", text)
+        text <- gsub(" court", " ct", text)
+        text <- gsub(" drive", " dr", text)
+        text <- gsub(" junction", " jct", text)
+        text <- gsub(" place", " pl", text)
+        text <- gsub(" road", " rd", text)
+        text <- gsub(" route", " rte", text)
+        text <- gsub(" square", " sq", text)
+        text <- gsub(" street", " st", text)
+        text <- gsub(" apartment", " apt", text)
+        text <- gsub(" building", " bldg", text)
     }
 
     return(text)
