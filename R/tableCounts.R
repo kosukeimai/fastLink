d7dd407ddfd9aa3ff5ec8e001e77acb72015a264
tedenamorado <tede@princeton.edu>
2018-05-08 19:25:08 -0400

    fix tableCounts

--- R/tableCounts.R
+++ R/tableCounts.R
@@ -158,7 +158,7 @@ tableCounts <- function(gammalist, nobs.a, nobs.b, n.cores = NULL) {
     na.data.new[na.data.new == 4] <- NA
     data.new <- cbind(na.data.new, data.new.1[, nc])
     colnames(data.new)[nc] <- "counts"
-    data.new <- data.new[data.new$counts > 0, ]
+    data.new <- data.new[data.new[, nc] > 0, ]
     class(data.new) <- c("fastLink", "tableCounts")
     return(data.new)
     
